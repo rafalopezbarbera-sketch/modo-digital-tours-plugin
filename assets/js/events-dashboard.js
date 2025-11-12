f6add50a12e857d2fc2646e8b1f590a6e346e64c
rafalopezbarbera-sketch <rafa.lopez.barbera@gmail.com>
2025-11-12 10:41:40 +0100

    Create events-dashboard.js

---
+++ assets/js/events-dashboard.js
@@ -0,0 +1,10 @@
+// Simple: obtiene eventos y los pinta
+jQuery(document).ready(function($) {
+  function getEvents() {
+    $.get(ajaxurl, {action: 'mdt_get_events'}, function(data) {
+      $('#mdt-events-app').html(data);
+    });
+  }
+  getEvents();
+  // TODO: implementar popups/CRUD con AJAX para crear/editar/borrar
+});
