940a37cfefe2891fb49e73f0938caf23fe4b5360
rafalopezbarbera-sketch <rafa.lopez.barbera@gmail.com>
2025-11-12 10:38:11 +0100

    Delete view directory

--- view/frontend/calendar.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$counter = isset($params['counter']) ? $params['counter'] : rand(1000, 9999);
-?>
-<div id="mdt-wrapper-<?php echo esc_attr($counter); ?>" class="mdt-wrapper" data-counter="<?php echo esc_attr($counter); ?>">
-    <!-- El JS montará la UI "por steps" dentro del root -->
-    <div class="mdt-root" data-counter="<?php echo esc_attr($counter); ?>"></div>
-</div>
--- view/frontend/list-inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$counter = isset($params['counter']) ? $params['counter'] : rand(1000, 9999);
-?>
-<div id="mdt-list-<?php echo esc_attr($counter); ?>" class="mdt-list-wrapper" data-counter="<?php echo esc_attr($counter); ?>">
-    <div class="mdt-list-root" data-counter="<?php echo esc_attr($counter); ?>"></div>
-</div>
